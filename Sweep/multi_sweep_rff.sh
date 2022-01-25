cb92d51228f6b6b4ac7013b475997b0fedf40edf
ant-stephenson <ant-stephenson@users.noreply.github.com>
2022-01-25 15:36:50 +0000

    add args/change params

--- Sweep/multi_sweep_rff.sh
+++ Sweep/multi_sweep_rff.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0:0:10
+#SBATCH --time=0:10:00
 #SBATCH --mem=100M
-#SBATCH --array=100-149
+#SBATCH --array=0-1
 
-srun multi_sweep_rff --param_idx=$SLURM_ARRAY_TASK_ID
+srun multi_sweep_rff --param_idx=$SLURM_ARRAY_TASK_ID --verbose="True" --NO_TRIALS=1 --significance_threshold=0.1

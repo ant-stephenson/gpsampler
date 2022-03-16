ba2ecff8420bc54426dcc0a29df7e5dee1072011
ll20823 <ll20823@bp1-login01.data.bp.acrc.priv>
2022-03-16 13:17:41 +0000

    bug fixes and reformatting

--- Sweep/rff.py
+++ Sweep/rff.py
@@ -9,24 +9,32 @@ from typing import Tuple, Optional
 
 rng = np.random.default_rng()
 
-k_true = lambda sigma, l, xp, xq : sigma*np.exp(-0.5*np.dot(xp-xq,xp-xq)/l**2) #true kernel
-z = lambda omega, D, x : np.sqrt(2/D)*np.ravel(np.column_stack((np.cos(np.dot(omega, x)),np.sin(np.dot(omega, x))))) #random features
-f = lambda omega, D, w, x : np.sum(w*z(omega, D, x)) #GP approximation
+
+def k_true(sigma, l, xp, xq): return sigma * \
+    np.exp(-0.5*np.dot(xp-xq, xp-xq)/l**2)  # true kernel
+def z(omega, D, x): return np.sqrt(2/D)*np.ravel(np.column_stack(
+    (np.cos(np.dot(omega, x)), np.sin(np.dot(omega, x)))))  # random features
+
+
+def f(omega, D, w, x): return np.sum(w*z(omega, D, x))  # GP approximation
+
 
 def estimate_rff_kernel(X: np.ndarray, D: int, ks: float, l: float) -> np.ndarray:
-    N,d = X.shape
+    N, d = X.shape
     cov_omega = np.eye(d)/l**2
     omega = rng.multivariate_normal(np.zeros(d), cov_omega, D//2)
     Z = np.array([z(omega, D, xx) for xx in X])*np.sqrt(ks)
     approx_cov = np.inner(Z, Z)
     return approx_cov
 
-def construct_kernels(l: float, b:float=1.0) -> gpytorch.kernels.Kernel:
+
+def construct_kernels(l: float, b: float = 1.0) -> gpytorch.kernels.Kernel:
     kernel = gpytorch.kernels.RBFKernel()
     kernel.lengthscale = l
     kernel = gpytorch.kernels.ScaleKernel(kernel)
     kernel.outputscale = b
     return kernel
+
 
 def approx_extreme_eigs(X, noise_var=None):
     max_eig = X.shape[0]
@@ -36,7 +44,8 @@ def approx_extreme_eigs(X, noise_var=None):
         min_eig = 1/max_eig
     return min_eig, max_eig
     raise NotImplementedError
-        
+
+
 def matsqrt(X, J, Q, reg=1e-6):
     """Calculates the matrix sqrt of a symmetric matrix X using method 3 in
     Hale2008. Note that this implementation is not computationally efficient as
@@ -54,9 +63,9 @@ def matsqrt(X, J, Q, reg=1e-6):
     """
     n = X.shape[0]
     I = np.eye(n)
-    m,M = approx_extreme_eigs(X, reg)
+    m, M = approx_extreme_eigs(X, reg)
     k2 = m/M
-    Kp = ellipk(1- k2)
+    Kp = ellipk(1 - k2)
     # for N in range(5,25,5):
     for N in [Q]:
         t = 1j * (np.arange(1, N + 1) - 0.5) * Kp / N
@@ -72,14 +81,16 @@ def matsqrt(X, J, Q, reg=1e-6):
         S = -2 * Kp * np.sqrt(m) / (np.pi * N) * X @ S
     return S
 
+
 def estimate_ciq_kernel(X, J, Q, ks, l, nv=None) -> np.ndarray:
     kernel = construct_kernels(l, ks)
     n, d = X.shape
     J = int(np.sqrt(n) * np.log(n))
     Q = int(np.log(n))
-    K = kernel(X).detach().numpy()
+    K = kernel(torch.tensor(X)).detach().numpy()
     rootK = matsqrt(K, J, Q, nv)
-    return rootK
+    return np.real(rootK @ rootK)
+
 
 def generate_ciq_data(n: int, xmean: np.ndarray, xcov_diag: np.ndarray, noise_var: float, kernelscale: float, lenscale: float, J: int, Q: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """ Generates a data sample from a MVN and a sample from an approximate GP
@@ -107,10 +118,11 @@ def generate_ciq_data(n: int, xmean: np.ndarray, xcov_diag: np.ndarray, noise_va
     u = rng.standard_normal(n)
 
     kernel = construct_kernels(lenscale, kernelscale)
-    solves, weights, _, _ = contour_integral_quad(kernel(x).evaluate_kernel(), torch.tensor(u), max_lanczos_iter=J, num_contour_quadrature=Q)
+    solves, weights, _, _ = contour_integral_quad(kernel(x).evaluate_kernel(
+    ), torch.tensor(u), max_lanczos_iter=J, num_contour_quadrature=Q)
     f = (solves * weights).sum(0).detach().numpy()
     noisy_sample = f + rng.normal(0.0, np.sqrt(noise_var), n)
-    return x, sample, noisy_sample 
+    return x, sample, noisy_sample
 
 
 def generate_rff_data(n: int, xmean: np.ndarray, xcov_diag: np.ndarray, noise_var: float, kernelscale: float, lenscale: float, D: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -132,20 +144,23 @@ def generate_rff_data(n: int, xmean: np.ndarray, xcov_diag: np.ndarray, noise_va
     assert D % 2 == 0
     input_dim = xmean.shape[0]
     assert input_dim == xcov_diag.shape[0]
-    
+
     xcov = np.diag(xcov_diag)
     x = rng.multivariate_normal(xmean, xcov, n)
 
-    cov_omega = np.eye(input_dim)/lenscale**2 #covariance matrix for fourier transform of kernel
-    omega = rng.multivariate_normal(np.zeros(input_dim), cov_omega, D//2) #sample from kernel spectral density
-    w = rng.standard_normal(D) #sample weights
+    # covariance matrix for fourier transform of kernel
+    cov_omega = np.eye(input_dim)/lenscale**2
+    # sample from kernel spectral density
+    omega = rng.multivariate_normal(np.zeros(input_dim), cov_omega, D//2)
+    w = rng.standard_normal(D)  # sample weights
 
-    my_f = partial(f, omega, D, w) #GP approx as function of only x
+    my_f = partial(f, omega, D, w)  # GP approx as function of only x
     sample = np.array([my_f(xx) for xx in x])*np.sqrt(kernelscale)
     noisy_sample = sample + rng.normal(0.0, np.sqrt(noise_var), n)
     return x, sample, noisy_sample
 
-def sample_rff_from_x(x: np.ndarray, sigma: float,noise_var: float,l: float, rng: np.random.Generator, D: int) -> Tuple[np.ndarray, np.ndarray]:
+
+def sample_rff_from_x(x: np.ndarray, sigma: float, noise_var: float, l: float, rng: np.random.Generator, D: int) -> Tuple[np.ndarray, np.ndarray]:
     """ Generates sample from approximate GP using RFF method at points x
 
     Args:
@@ -159,7 +174,7 @@ def sample_rff_from_x(x: np.ndarray, sigma: float,noise_var: float,l: float, rng
     Returns:
         Tuple[np.ndarray, np.ndarray]: Approx. GP draw; 1D array of length n and approx cov
     """
-    n,d = x.shape
+    n, d = x.shape
     cov_omega = np.eye(d)/l**2
     omega = rng.multivariate_normal(np.zeros(d), cov_omega, D//2)
 
@@ -171,10 +186,11 @@ def sample_rff_from_x(x: np.ndarray, sigma: float,noise_var: float,l: float, rng
 
     my_z = partial(z, omega, D)
     Z = np.array([my_z(xx) for xx in x])*np.sqrt(sigma)
-    approx_cov = np.inner(Z,Z)
+    approx_cov = np.inner(Z, Z)
     return y_noise, approx_cov
 
-def sample_ciq_from_x(x: np.ndarray, sigma: float,noise_var: float,l: float, rng: np.random.Generator, J: int, Q: Optional[int]=None) -> Tuple[np.ndarray, np.ndarray]:
+
+def sample_ciq_from_x(x: np.ndarray, sigma: float, noise_var: float, l: float, rng: np.random.Generator, J: int, Q: Optional[int] = None) -> Tuple[np.ndarray, np.ndarray]:
     """ Generates sample from approximate GP using RFF method at points x
 
     Args:
@@ -192,25 +208,27 @@ def sample_ciq_from_x(x: np.ndarray, sigma: float,noise_var: float,l: float, rng
     u = rng.standard_normal(n)
 
     kernel = construct_kernels(l, sigma)
-    solves, weights, _, _ = contour_integral_quad(kernel(torch.tensor(x)).evaluate_kernel(), torch.tensor(u.reshape(-1,1)), max_lanczos_iter=J, num_contour_quadrature=Q)
+    solves, weights, _, _ = contour_integral_quad(kernel(torch.tensor(x)).evaluate_kernel(
+    ), torch.tensor(u.reshape(-1, 1)), max_lanczos_iter=J, num_contour_quadrature=Q)
     f = (solves * weights).sum(0).detach().numpy().squeeze()
     y_noise = f + rng.normal(0.0, np.sqrt(noise_var), n)
     approx_cov = estimate_ciq_kernel(x, J, Q, sigma, l)
-    return  y_noise, approx_cov
+    return y_noise, approx_cov
+
 
 if __name__ == '__main__':
-    N = 1000 # no. of data points
-    d = 10 #input (x) dimensionality
-    D = 1000 #no.of fourier features
-    l = 1.1 #lengthscale
-    sigma = 0.7 #kernel scale
-    noise_var = 0.2 #noise variance
-    
+    N = 1000  # no. of data points
+    d = 10  # input (x) dimensionality
+    D = 1000  # no.of fourier features
+    l = 1.1  # lengthscale
+    sigma = 0.7  # kernel scale
+    noise_var = 0.2  # noise variance
+
     xmean = np.zeros(d)
     xcov_diag = np.full(d, 1.0/d)
-    
+
     print(
-    """
+        """
 data_size %d
 xmean %s
 xcov_diag %s
@@ -218,21 +236,22 @@ noise_var %.2f
 kernelscale %.2f
 lenscale %.2f
     """
-            % (
-                N, 
-                str(xmean), 
-                str(xcov_diag), 
-                noise_var, 
-                sigma, 
-                l
-            )
+        % (
+            N,
+            str(xmean),
+            str(xcov_diag),
+            noise_var,
+            sigma,
+            l
         )
-    
-    x, sample, noisy_sample = generate_rff_data(N, xmean, xcov_diag, noise_var, sigma, l, D)
+    )
+
+    x, sample, noisy_sample = generate_rff_data(
+        N, xmean, xcov_diag, noise_var, sigma, l, D)
     # np.savetxt("x.out.gz", x)
     # np.savetxt("sample.out.gz", sample)
     # np.savetxt("noisy_sample.out.gz", noisy_sample)
-    
+
     import resource
     mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print("samples have been generated")

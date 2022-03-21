2c8d47ae0a5aa27732d3ea28586d85c5b9b56587
ll20823 <ll20823@bp1-login01.data.bp.acrc.priv>
2022-03-21 09:47:14 +0000

    fixed dir change w submodule and method arg for script

--- Sweep/multi_sweep_rff.py
+++ Sweep/multi_sweep_rff.py
@@ -8,15 +8,18 @@ if __name__ == "__main__":
     parser.add_argument("--NO_TRIALS", default=1, type=int)
     parser.add_argument("--significance_threshold", default=0.1, type=float)
     parser.add_argument("--ncpus", default=1, type=int)
+    parser.add_argument("--method", default="rff", type=str)
 
     args = parser.parse_args()
 
-    param_set = {k:v for (k,v) in zip(sweep_rff.default_param_set.keys(), sweep_rff.param_sets[args.param_idx])}
+    param_set = {k: v for (k, v) in zip(
+        sweep_rff.default_param_set.keys(), sweep_rff.param_sets[args.param_idx])}
 
     sweep_rff.run_sweep(
         **param_set,
-        verbose = args.verbose,
+        verbose=args.verbose,
         NO_TRIALS=args.NO_TRIALS,
         significance_threshold=args.significance_threshold,
-        ncpus=args.ncpus
+        ncpus=args.ncpus,
+        method=args.method,
     )

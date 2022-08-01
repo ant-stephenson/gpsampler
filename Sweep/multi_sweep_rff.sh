66bcd9e4c8775bc02083f4f9b5c94d0116b64cbf
ll20823 <ll20823@bp1-login01.data.bp.acrc.priv>
2022-08-01 14:04:01 +0100

    added job id, fixed bug w python version

--- Sweep/multi_sweep_rff.sh
+++ Sweep/multi_sweep_rff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=sweep_rff
+#SBATCH --job-name=sweep_rff_precon
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -17,6 +17,8 @@ source $SCRIPT_DIR/projenv/bin/activate
 
 module load lang/python/anaconda/3.8.5-2021-AM
 
-python $SCRIPT_DIR/rff/Sweep/multi_sweep_rff.py --param_idx=$SLURM_ARRAY_TASK_ID --verbose="True" --NO_TRIALS=1000 --significance_threshold=0.05 --ncpus=$SLURM_CPUS_PER_TASK --method=ciq
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/lang/anaconda.3.8.5-2021-AM/lib
+
+python $SCRIPT_DIR/rff/Sweep/multi_sweep_rff.py --job_id $SLURM_ARRAY_JOB_ID --param_idx=$SLURM_ARRAY_TASK_ID --verbose="True" --NO_TRIALS=1000 --significance_threshold=0.1 --ncpus=$SLURM_CPUS_PER_TASK --method=ciq
 
 deactivate

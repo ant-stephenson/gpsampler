f6dff8a692a32f907550caa109de1f84d8a090cf
ant-stephenson <ant-stephenson@users.noreply.github.com>
2022-09-14 11:29:31 +0100

    made plots prettier

--- Sweep/analyse_sweep_output.py
+++ Sweep/analyse_sweep_output.py
@@ -2,70 +2,142 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from gpybench.plotting import plot_yeqx, save_fig, LaTeX
-from gpybench.utils import isnotebook, check_exists
-from typing import Annotated, Final
+from gpybench.plotting import save_fig, LaTeX
+from gpybench.utils import isnotebook
+from gpytools.utils import ordermag
 from nptyping import NDArray
 from pathlib import Path
 
-import re
-import inspect
-
+from typing import Annotated, Final
+ 
 #%%
 if isnotebook():
-    path = Path(".")
+    path = Path("../..")
 else:
     path = Path(".")
-#%%
-method = "ciq"
-sweep = pd.read_csv(path.joinpath(f"output_sweep_{method}_1.csv"))
-sweep = sweep.sort_values(by=["N","D","l"])
-sweep_grp = sweep.groupby(["d", "l", "sigma", "noise_var"])
+#%% script params
+method = "rff"
+job_id = 2580211#2703849#2580211#2686645#
+param_idx = 1
+sig_thresh = 0.1
+with_pre = False
+#compute approximate confidence interval on the rejection rate, by using the
+#following argument:
+# E[r] = q, rhat ~ q; var(rhat) = q(1-q)/N; var(rhat)_hat ~ rhat(1-rhat)/N
+# q = 1/2*(alpha + (1-beta)), rhat = 1/2(alpha + (1-betahat))
+# betahat = 1 - 2rhat + alpha
+# varrhat = rhat(betahat-alpha)/N = rhat(1-2rhat)/N
+# assume N = 1000 (check file)
+# sweep.loc[:, "rsigma"] = 2 * np.sqrt(sweep.reject * (1-sweep.reject) / 1000)
 
-grp = list(sweep_grp.groups.keys())[0]
-title = [f"{k}:{v}" for k,v in zip(sweep_grp.keys, grp)]
-#%% - plot error as function of no. RFF (logscales). err = ||K-Krff||_F
-ax1 = sns.lineplot(x="D", y="err", data=sweep, hue="N")
-ax1.set(xscale="log", yscale="log")
-ax1.set_title(title)
-# save_fig(path, f"logerr-logD_byN_{method}", suffix="jpg", show=True)
-#%% empirically estimate the convergence of r wrt D? e.g. D ~ nlogn or n^2 or ... i.e. find the first D for each N s.t. the observed rejection rate is within some small distance (1e-2 for now; arbitrary need fluctuation analysis) of the significance level, taking the average over lengthscales:
+#BETTER, just use
+ci95 = 1.96*np.sqrt(0.1*0.9/1000)
+# on the grounds that for N=1000 => CLT, so this is ~95%ci
+
+# get data
+sweep = pd.read_csv(path.joinpath(f"output_sweep_{method}_{param_idx}_{job_id}.csv"))
+
+# sort out data
+sweep = sweep.rename({"N":"n"}, axis=1)
+sweep = sweep.sort_values(by=["n","D","l"])
+# if there are duplicates, average over them
+sweep_grp = sweep.groupby(sweep.columns.difference(["reject","err"]).tolist())
+sweep = sweep_grp.mean()
+# RIP index
+sweep = sweep.reset_index()
+
+# only keep 0.1 and 2.0 for now?
+sweep = sweep.query("l in [0.1,1.0]")
+
+if method == "ciq":
+    sweep = sweep.rename({"D":"J"}, axis=1)
+    fidel_param = "J"
+    if with_pre:
+        order_f = lambda n: n**(3/8) * np.log(n)  
+    else:
+        order_f = lambda n: np.sqrt(n) * np.log(n)
+else:
+    fidel_param = "D"
+    order_f = lambda n: n**(3/2) * np.log(n)
+
+#%% empirically estimate the convergence of r wrt D? e.g. D ~ nlogn or n^2 or
+#... i.e. find the first D for each N s.t. the observed rejection rate is within
+#some small distance (1e-2 for now; arbitrary need fluctuation analysis) of the
+#significance level, taking the average over lengthscales:
 crossing_Ds = dict()
-for idx, grp_df in sweep.groupby(["N"]):
-    avg_r_by_D = grp_df.groupby("D").reject.mean()
-    crossing_Ds[idx] = (np.abs(avg_r_by_D - 0.1) > 1e-2).idxmin()
-#%% - plot rejection rate as function of no. RFF (logscales)
+for l in sweep.l.unique():
+    crossing_Ds[l] = dict()
+    for idx, grp_df in sweep.loc[sweep.l == l,:].groupby(["n"]):
+        avg_r_by_D = grp_df.groupby(fidel_param).reject.mean()
+        crossing_Ds[l][idx] = (np.abs(avg_r_by_D - 0.1) > 1e-2).idxmin()
 
-def plot_reject_by_D(df: pd.DataFrame):
-    palette = sns.color_palette(palette="flare", n_colors=df.N.nunique())
-    ax2 = sns.lineplot(x="D", y="reject", data=df, hue="N", palette=palette)
-    ax2.set(xscale="log", yscale="log")
-    # Put the legend out of the figure
-    ax2.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    maxy = df.reject.max()
-    
-    order_f = lambda n: n ** (1/2) * np.log10(n)
-    ax2.vlines(order_f(df.N.unique()), ymin=0, ymax=maxy, colors=palette)
-    ax2.vlines(crossing_Ds.values(), ymin=0, ymax=maxy, colors=palette, ls="dotted")
-    sig_thresh = 0.1
-    ax2.axhline(sig_thresh, ls='--')
-    ax2.axhline(sig_thresh + 1e-2, ls='--', color="green")
-    ax2.axhline(sig_thresh - 1e-2, ls='--', color="green")
+import matplotlib.pyplot as plt
+palette = sns.color_palette(palette="flare", n_colors=sweep.n.nunique())
 
-    # eps = eps = np.exp(-sweep.noise_var/np.sqrt(sweep.N) * (sweep.D-1) - np.log(sweep.N)-5)
-    # ax2.plot(sweep.D, eps+0.1)
+#%% poster plots #1
 
-    str_f = re.findall(r"(?<=\:).*", inspect.getsource(order_f))[0]
-    ax2.set_title(str_f)
-    # save_fig(path, f"logreject-logD_byN_{method}", suffix="jpg", show=True, size_inches=(12,8))
+def conv_plot(df, xlabel, ylabel="reject"):
 
-sweep_sub = sweep_grp.get_group(grp)
-plot_reject_by_D(sweep)
-# %%- plot err vs reject (for particular value of l for now)
+    with LaTeX() as ltx:
+        ax = sns.relplot(
+        data=df,
+        x=xlabel, y=ylabel,
+        hue="n", col="l",
+        kind="line", palette=palette, col_wrap=2,
+        height=5, aspect=.75, facet_kws=dict(sharex=False),
+        )
+        ax.set(xscale="log", yscale="log")
+        ax.set(yticks=[0.1,0.5,1])
+        ax.set_yticklabels([0.1,0.5,1])
+
+        xticks = [ordermag(x) for x in np.geomspace(df.loc[:,xlabel].min(), df.loc[:,xlabel].max(), num=4)]
+
+        for _l,_ax in ax.axes_dict.items():
+            _ax.axhline(sig_thresh, ls='--')
+            _ax.axhline(sig_thresh + ci95, ls='--', color="green")
+            _ax.axhline(sig_thresh - ci95, ls='--', color="green")
+            _ax.axvline(1, ls='--', color="black")
+            _ax.patch.set_edgecolor('black')  
+            _ax.patch.set_linewidth('1') 
+            _ax.set_xticks(xticks)
+            _ax.set_ylim(top=1)
+
+    fig = plt.gcf()
+    title = method.upper()
+    if with_pre:
+        title = "P" + title
+    fig.suptitle(title)
+
+#%%
+conv_plot(sweep, fidel_param)
+save_fig(path, f"logreject-logD_byN_{method}_{param_idx}_{job_id}", suffix="pdf", show=True, dpi=600, overwrite=True)
+
+
+#%% Repeat plots but re-scale x-axis by sqrt(n)log(n) to reflect scaling
+#expectation
+rescaled_fidel = "$"+fidel_param+"/"+"\\bar{"+f"{fidel_param}"+"}"+"(n)$"
+sweep.loc[:, rescaled_fidel] = sweep.loc[:, fidel_param]/order_f(sweep.n)
+
+#%%
+conv_plot(sweep, rescaled_fidel)
+save_fig(path, f"logreject-logD_byN_{method}_{param_idx}_{job_id}_rescaled", suffix="pdf", show=True, dpi=600, overwrite=True)
+#%%
+raise Exception("End script")
+#group by parameter values
+# sweep_grp = sweep.groupby(["d", "l", "sigma", "noise_var"])
+
+# grp = list(sweep_grp.groups.keys())[0]
+# title = [f"{k}:{v}" for k,v in zip(sweep_grp.keys, grp)]
+#%% - plot error as function of no. RFF (logscales). err = ||K-Krff||_F
+# ax1 = sns.lineplot(x="D", y="err", data=sweep, hue="N")
+# ax1.set(xscale="log", yscale="log")
+# ax1.set_title(title)
+# save_fig(path, f"logerr-logD_byN_{method}", suffix="jpg", show=True)
+# %- plot err vs reject (for particular value of l for now)
 ax3 = sns.lineplot(x="err", y="reject", hue="N", marker="D", data=sweep_sub)
 ax3.set(xscale="log", yscale="log")
 save_fig(path, f"logreject-logerr_byN_{method}", suffix="jpg", show=True)
-#%% not actually 1...want a placeholder as we want to just state that the
+#% not actually 1...want a placeholder as we want to just state that the
 #function is shape-preserving
 M: int = 1 #!
 N: int = 1
@@ -79,17 +151,17 @@ def transform_cols(df: NDArray[(M,N)], transforms: dict) -> NDArray[(M,N)]:
     id = lambda x: x
     _transforms = {**{k:id for k in df.columns}, **transforms}
     return df.transform(_transforms)
-# %% fit linear reg. models: log(reject) ~ log(D | N)
+# % fit linear reg. models: log(reject) ~ log(D | N)
 transforms = {"reject": np.log}
-ax4 = sns.lmplot(x="D", y="reject", data=transform_cols(sweep, transforms), col="N", logx=True, col_wrap=3)
+ax4 = sns.lmplot(x=fidel_param, y="reject", data=transform_cols(sweep, transforms), col="N", logx=True, col_wrap=3)
 ax4.set(xscale="log")
 save_fig(path, f"logreject-logD_byN_reg_{method}", suffix="jpg", show=True)
-# %% fit linear reg. models: log(err) ~ log(D | N)
+# % fit linear reg. models: log(err) ~ log(D | N)
 transforms = {"err": np.log}
-ax5 = sns.lmplot(x="D", y="err", data=transform_cols(sweep, transforms), col="N", logx=True, col_wrap=3)
+ax5 = sns.lmplot(x=fidel_param, y="err", data=transform_cols(sweep, transforms), col="N", logx=True, col_wrap=3)
 ax5.set(xscale="log")
 save_fig(path, f"logerr-logD_byN_reg_{method}", suffix="jpg", show=True)
-# %% fit linear reg. models: log(reject) ~ log(err | N)
+# % fit linear reg. models: log(reject) ~ log(err | N)
 transforms = {"reject": np.log}
 ax6 = sns.lmplot(x="err", y="reject", data=transform_cols(sweep, transforms), col="N", logx=True, col_wrap=3)
 ax6.set(xscale="log")

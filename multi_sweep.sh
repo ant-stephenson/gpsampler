cc719b16b347745f66f1865bb17a2808254d7bbd
ll20823 <ll20823@bp1-login01.data.bp.acrc.priv>
2022-10-20 09:59:51 +0100

    fixed path w name change (and run chol)

--- multi_sweep.sh
+++ multi_sweep.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --time=200:00:00
 #SBATCH --mem-per-cpu=15G
-#SBATCH --array=2
+#SBATCH --array=1
 
 SCRIPT_DIR=/user/work/ll20823/mini-project
 
@@ -18,6 +18,6 @@ module load lang/python/anaconda/3.8.5-2021-AM
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/lang/anaconda.3.8.5-2021-AM/lib
 
-python $SCRIPT_DIR/rff/Sweep/multi_sweep_rff.py --job_id $SLURM_ARRAY_JOB_ID --param_idx=$SLURM_ARRAY_TASK_ID --verbose="True" --NO_TRIALS=1000 --significance_threshold=0.1 --ncpus=$SLURM_CPUS_PER_TASK --method=ciq --pre="False"
+python $SCRIPT_DIR/rff/multi_sweep.py --job_id $SLURM_ARRAY_JOB_ID --param_idx=$SLURM_ARRAY_TASK_ID --verbose="True" --NO_TRIALS=1000 --significance_threshold=0.1 --ncpus=$SLURM_CPUS_PER_TASK --method=chol --pre="True"
 
 deactivate

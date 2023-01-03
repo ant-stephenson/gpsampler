3f1651b6e79919fae6cb8804c5dabac0d331d921
ant-stephenson <ant-stephenson@users.noreply.github.com>
2023-01-03 13:38:47 +0000

    alt. cg sampler

--- test_cg_sampler.py
+++ test_cg_sampler.py
@@ -1,11 +1,12 @@
 #%%
 from scipy import linalg
 from gpybench.utils import get_off_diagonal, numpify, print_mean_std, isnotebook
-from gpytools.maths import estimate_rff_kernel, estimate_rff_kernel_inv, f_gp_approx, msqrt, k_se
+from gpytools.maths import estimate_rff_kernel, estimate_rff_kernel_inv, f_gp_approx, msqrt,invmsqrt, k_se
 import gpybench.plotting as gplt
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
+from scipy.sparse import diags
 #%%
 n = 200
 d = 2
@@ -14,7 +15,7 @@ sigma = 1.0
 nv = 1e-3
 
 x = np.random.randn(n,d)/np.sqrt(d)
-K = k_se(x, ls=l,sigma=sigma)
+K = k_se(x, x, ls=l,sigma=sigma)
 Ke = K + np.eye(n) * nv
 
 rootK = msqrt(Ke)
@@ -63,34 +64,65 @@ def conj_grad(A,x0,b,u=None, loss=None, fprime=None):
         k += 1
     print(f"Terminated after {k} iterations.")
     return x, y, P, D
-        
-# %% check usual CG works
-tmp, smple, _, _ = conj_grad(rootK, np.random.randn(n), y)
-linalg.norm(tmp - u)
 
-# out = optimize.fmin_cg(lambda x: 0.5*np.inner(rootK @ x - y, rootK @ x - y),
-# np.random.randn(n), fprime=lambda x: rootK.T @ (rootK @ x - y))
-# z = inv(rootK) @ y
+# %% check usual CG works w rootK @ u - y
+uhat, smple, P0, D0 = conj_grad(rootK, np.random.randn(n), y)
+print(f"CG error: {linalg.norm(uhat - u)}")
+linalg.norm(rootK - P0 @ np.diag(D0) @ P0.T)
+
+#%%
+out = optimize.fmin_cg(lambda x: 0.5*np.inner(rootK @ x - y, rootK @ x - y),
+np.random.randn(n), fprime=lambda x: rootK.T @ (rootK @ x - y), retall=True)
 # %% check sampling
 x0 = np.zeros(n)
-b = u/2#np.sign(np.random.uniform(-1,1,n))
+b = np.random.standard_normal(n)#np.sign(np.random.uniform(-1,1,n))
 
+# Ke @ x - b
 _, ycg, P, D = conj_grad(Ke, x0, b, u/2)
-print(f"CG error (1): {linalg.norm(y-ycg)}")
+print(f"CG sample error (1): {linalg.norm(y-ycg)}")
  
-ycg_test = Ke @ P @ np.sqrt(np.asarray(D))**(-1) * u/2
-print(f"CG error (2): {linalg.norm(y-ycg_test)}")
+ycg_test = P @ np.sqrt(np.asarray(D)) * u/2
+print(f"CG sample error (2): {linalg.norm(y-ycg_test)}")
 # %% RFF benchmark
 Krff = estimate_rff_kernel(x, int(n**2*np.log(n)), l, sigma)
 Krffe = Krff + nv * np.eye(n)
 yrff = msqrt(Krffe) @ u
 rff_err = linalg.norm(yrff - y)
-print(f"RFF error: {rff_err}")
+print(f"RFF sample error: {rff_err}")
  
-# %% chol benchmark
+# %% chol benchmark - note: not that good...
 L = linalg.cholesky(Ke, lower=True)
 chol_err = linalg.norm(L @ u - y)
-print(f"Chol error: {chol_err}")
+print(f"Chol sample error: {chol_err}")
 # %% random var-matched benchmark
-print(f"Random error: {linalg.norm(u*np.sqrt(y.var()) - y)}")
+print(f"Random sample error: {linalg.norm(u*np.sqrt(y.var()) - y)}")
+# %%
+def cg_sampler(Q, m, z):
+    # z = np.random.randn(m)
+    x0 = np.zeros(m)
+    r0 = z - Q @ x0
+
+    beta = np.zeros(m)
+    alpha = np.zeros(m)
+
+    V = np.zeros((n,m))
+
+    beta[0] = linalg.norm(r0)
+    V[:,1] = r0/beta[0]
+
+    e1 = np.concatenate(([1],np.zeros(m-1, dtype=int)))
+
+    for j in range(1, m-1):
+        wj = Q @ V[:,j] - beta[j] * V[:,j-1]
+        alpha[j] = wj.T @ V[:,j]
+        beta[j+1] = linalg.norm(wj)
+        V[:,j+1] = wj/beta[j+1]
+
+    offset = [-1,0,1]
+    # not sure if we should exclude the first or last beta...
+    T = diags([beta[:-1],alpha,beta[:-1]],offset).toarray()
+    return x0 + beta[0] * V @ invmsqrt(T) @ e1
+#%%
+ycg2 = cg_sampler(Ke, n, u)
+print(f"CG sample error (3): {linalg.norm(y-Ke @ ycg2)}")
 # %%
